902d6c90fe9968a8acc10d6e0d5c15b0b88c6dc8
Sam_Huang <samtash1034@gmail.com>
2023-07-01 23:07:23 +0800

    ok

---
+++ src/main/java/com/sam/excelaccountingmanager/ExcelAccountingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sam.excelaccountingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelAccountingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelAccountingManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/excelaccountingmanager/ExcelAccountingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.excelaccountingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelAccountingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
